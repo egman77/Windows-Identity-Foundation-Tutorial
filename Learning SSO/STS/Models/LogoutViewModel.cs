2023d6f37186a28edd27a6ac839ca0a5ca43d161
何东华 <egman@qq.com>
2018-02-04 20:08:45 +0800

    完成代码的优化,以区分不同的功能

---
+++ Learning SSO/STS/Models/LogoutViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace STS.Models
+{
+    /// <summary>
+    /// 注销的视图模型
+    /// </summary>
+    public class LogoutViewModel
+    {
+        public List<string> LogoutUrls;
+        public string ReplyTo;
+    }
+}
